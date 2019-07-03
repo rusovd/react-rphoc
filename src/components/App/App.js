5bea4cad2c0c91bf371079f24dcb77a03006dade
Denys Rusov <denis.rusov@sandbx.co>
2019-07-04 00:00:46 +0300

    Modified ProductList(and route in App.js) to get the data from render props of ProductData

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import ProductList from "../ProductList";
 import ProductTable from "../ProductTable";
-import { BrowserRouter as Router, Route } from  'react-router-dom' 
-import NavBar from "../NavBar"
+import ProductData from "../ProductData";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "../NavBar";
 
 import "./App.css";
 
@@ -11,8 +12,16 @@ export default class App extends Component {
     return (
       <Router>
         <NavBar />
-        <Route path="/ProductList" component={ProductList}></Route>
-        <Route path="/ProductTable" component={ProductTable}></Route>
+        <Route
+          path="/ProductList"
+          render={() => (
+            <ProductData
+              render={({ products }) => 
+                <ProductList products={products} />}
+            />
+          )}
+        />
+        <Route path="/ProductTable" component={ProductTable} />
       </Router>
     );
   }
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,60 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { Component } from "react";
+import React from "react";
 import { Button, Table } from 'reactstrap'
 
 import "./ProductList.css";
 
-export default class ProductList extends Component {
-  state = {
-    products: []
-  };
+const ProductList = ({ products }) => {
+  return (
+    <>
+      <Table bordered hover responsive>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
 
-  getProducts = async () => {
-    return [
-      { id: 1, name: "Nescafe", price: 4.2 },
-      { id: 2, name: "Lipton", price: 4 },
-      { id: 3, name: "Coca-Cola", price: 1.1 },
-      { id: 4, name: "Sneakers", price: 0.9 },
-      { id: 5, name: "Milk", price: 1.1 },
-      { id: 6, name: "Eggs", price: 1.2 },
-      { id: 7, name: "Sugar", price: 1.5 },
-      { id: 8, name: "Brandy", price: 8.5 },
-      { id: 9, name: "Apple Juice", price: 2.1 }
-    ];
-  };
-
-  componentDidMount() {
-    this.getProducts().then(products => {
-      this.setState({
-        products
-      });
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <Table bordered hover responsive>
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th></th>
+          {products && products.map(product => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td><Button color="success">Add to Cart</Button></td>
             </tr>
-          </thead>
-          <tbody>
-            {this.state.products.map(product => (
-              <tr key={product.id}>
-                <td>{product.id}</td>
-                <td>{product.name}</td>
-                <td>{product.price}</td>
-                <td><Button color="success">Add to Cart</Button></td>
-              </tr>
-              ))}
-          </tbody>
-        </Table>       
-      </>
-    );
-  }
+            ))}
+        </tbody>
+      </Table>       
+    </>
+  );
 }
+
+export default ProductList
